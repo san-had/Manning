cc12d26fc4c3b48038421d304a5e41dcb88e9da3
shadhazi <sandor.hadhazi@qualysoft.com>
2020-01-20 09:28:46 +0100

    removed 3.1 version

--- Manning/EmptyExample/Startup.cs
+++ Manning/EmptyExample/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace KestrelWithHttps
+namespace EmptyExample
 {
     public class Startup
     {
